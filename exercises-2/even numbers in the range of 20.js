5d7ea9f467e3972bc1546a76f82daecf7995ca6a
dilshad-mk <diludilshad1001@gmail.com>
2025-11-14 09:45:11 +0530

    updated with new exercises

--- exercises-2/even numbers in the range of 20.js
+++ exercises-2/even numbers in the range of 20.js
@@ -1,6 +1,6 @@
-for (i=0; i<=20; i++){
+for (i=0; i<=20;i+=2){
 
-    if(i/2==0){
+
         console.log(i)
-    }
+    
 }
---
+++ exercises-3/divisable by 7.js
@@ -0,0 +1,10 @@
+let i=1;
+while(
+    i<=100
+){
+    if(i%7==0)
+    {
+        console.log(i);
+    }
+    i++;
+}
