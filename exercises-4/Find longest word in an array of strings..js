5bd70af4c89d9614bbdf2f95af777e9bac4b3a80
dilshad-mk <diludilshad1001@gmail.com>
2026-01-01 12:11:46 +0530

    added new exercises

--- exercises-4/Find longest word in an array of strings..js
+++ exercises-4/Find longest word in an array of strings..js
@@ -1,7 +1,7 @@
-let str ="welcome to js";
+let str =["welcome ","to", "js"];
 let largest='';
-let words=str.split(" ")
-for(let ele of words){
+// let words=str.split(" ")
+for(let ele of str){
     if (ele.length>largest.length){
         largest=ele
     }
